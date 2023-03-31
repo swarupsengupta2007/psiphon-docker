a67d4424c395ef0745b053062beba5b38f051e72
swarupsengupta2007 <swarupsengupta2007@gmail.com>
2023-04-01 04:14:56 +0530

    Updated for Psiphon-2.0.29 release

--- make.bash
+++ make.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TARGETS="linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6"
+VERSION=2.0.29
 
-sudo docker buildx build --build-arg TARGETS=${TARGETS} -t swarupsengupta2007/psiphon:2.0.23 \
+sudo docker buildx build --build-arg TARGETS=${TARGETS} --build-arg VERSION=${VERSION} -t swarupsengupta2007/psiphon:${VERSION} \
 -t swarupsengupta2007/psiphon:latest --platform ${TARGETS} . $1
