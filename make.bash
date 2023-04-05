e29b256f926be893e7581ccad5bca1141f04dbd2
swarupsengupta2007 <swarupsengupta2007@gmail.com>
2023-04-05 18:18:53 +0530

    Reset Dockerfile to use base version from which this project was based off. Will use build-args to build new images (make.bash)

--- make.bash
+++ make.bash
@@ -3,5 +3,9 @@
 TARGETS="linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6"
 VERSION=2.0.29
 
-sudo docker buildx build --build-arg TARGETS=${TARGETS} --build-arg VERSION=${VERSION} -t swarupsengupta2007/psiphon:${VERSION} \
--t swarupsengupta2007/psiphon:latest --platform ${TARGETS} . $1
+sudo docker buildx build \
+				--build-arg TARGETS=${TARGETS} \
+				--build-arg VERSION=${VERSION} \
+				-t swarupsengupta2007/psiphon:${VERSION} \
+				-t swarupsengupta2007/psiphon:latest \
+				--platform ${TARGETS} . $1
