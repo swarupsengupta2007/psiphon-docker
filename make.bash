4fd631a9d33d7099346d6068f4a0a4b4dadd3341
swarupsengupta2007 <swarupsengupta2007@gmail.com>
2024-05-04 19:32:40 +0530

    modified make.bash to take arguments as environemt variables

--- make.bash
+++ make.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-TARGETS="linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6"
-VERSION=2.0.30
-GO_VERSION=1.20
+MK_TARGETS=${TARGETS:-"linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6"}
+MK_VERSION=${VERSION:-2.0.30}
+MK_GO_VERSION=${GO_VERSION:-1.20}
 
 sudo docker buildx build \
-				--build-arg TARGETS=${TARGETS} \
-				--build-arg VERSION=${VERSION} \
-				--build-arg GO_VERSION=${GO_VERSION} \
-				-t swarupsengupta2007/psiphon:${VERSION} \
+				--build-arg TARGETS=${MK_TARGETS} \
+				--build-arg VERSION=${MK_VERSION} \
+				--build-arg GO_VERSION=${MK_GO_VERSION} \
+				-t swarupsengupta2007/psiphon:${MK_VERSION} \
 				-t swarupsengupta2007/psiphon:latest \
-				--platform ${TARGETS} . $1
+				--platform ${MK_TARGETS} . $1
